d39bd75f905c1c61f7a94ac5b1d5c6c135d32eca
MosesAdamu <zegnovini@yahoo.com>
2018-12-01 02:28:09 +0000

    modified statistics page

--- app/src/main/java/com/hackapi/childsafe/MainActivity.java
+++ app/src/main/java/com/hackapi/childsafe/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hackapi.childsafe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.onesignal.OneSignal;
 
